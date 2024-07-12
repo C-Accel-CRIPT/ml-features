8020721ec0ff4c525c7122a4f954ee34719b4fff
Wrpicker <111399403+Wrpicker@users.noreply.github.com>
2024-07-12 02:50:48 -0700

    Added Data Saving Feature
    Added the feature to save material data in a csv file

--- CRIPT_EXPORT.py
+++ CRIPT_EXPORT.py
@@ -1,10 +1,11 @@
-import cript
-import csv
+from cript import *
 from enum import Enum
 import numpy as np
-import sklearn.preprocessing
+from dotenv import load_dotenv
 
-def ML_Data_Save(SearchProperties, FeatureProperties, LabelProperty):
+""" def ML_Data_Save(SearchProperties, FeatureProperties, LabelProperty):
+
+    import csv
 
     materials_list = cript.search(*Insert code to search for materials with Properties*)
 
@@ -26,7 +27,7 @@ def ML_Data_Save(SearchProperties, FeatureProperties, LabelProperty):
         test_writer.writerows(test data)
 
         train_file.close()
-        test_file.close()
+        test_file.close() """
 
 def sklearn_process(raw_data, preprocess):
 
@@ -64,7 +65,6 @@ def tensorflow_process(raw_data, preprocess):
     
     pass
 
-
 def pytorch_process(raw_data, preprocess):
 
     import torch
@@ -83,28 +83,30 @@ data_process_functions = {
     ML_Library.PYTORCH : pytorch_process}
 
 #Preprocess = [Normalization, Outlier Removal, One Hot Encoding]
-def ML_Data_Process(SearchParameter, FeatureProperties, LabelProperties, Library, Preprocess=False, limit=1000):
+
+def Mat_Search(Properties, SearchParameter, limit, getname = False):
     
-    #SKLEARN uses array like objects for fit model
-
-    #TENSORFLOW uses pandas.core.frame.DataFrame
-
-    #PYTORCH uses torchvision.datasets.mnist.FasionMNIST
-
     load_dotenv()
 
     result_list = Search(node="Material", q=SearchParameter, filters={"limit": limit})
 
     get_smile = False
 
-    if "smiles" in FeatureProperties or "smiles" in LabelProperties:
+    if "smiles" in Properties:
         get_smile = True
+
+    if "name" in Properties:
+        getname = True
 
     material_list = []
 
     for material in result_list:
 
         mat_dic = {}
+
+        if getname == True:
+
+            mat_dic["name"] = material["name"]
         
         if get_smile == True:
             
@@ -112,7 +114,7 @@ def ML_Data_Process(SearchParameter, FeatureProperties, LabelProperties, Library
 
         for property in material["property"]:
 
-            if (property["key"] in FeatureProperties or property["key"] in LabelProperties) and property['value'] != '':
+            if (property["key"] in Properties) and property['value'] != '':
                 
                 try:
                     mat_dic[property["key"]] = property["value"]
@@ -124,13 +126,7 @@ def ML_Data_Process(SearchParameter, FeatureProperties, LabelProperties, Library
 
         if len(mat_dic) > 0:
             
-            for prop in FeatureProperties:
-
-                if prop not in mat_dic.keys():
-                    add = False
-                    break
-            
-            for prop in LabelProperties:
+            for prop in Properties:
 
                 if prop not in mat_dic.keys():
                     add = False
@@ -139,10 +135,46 @@ def ML_Data_Process(SearchParameter, FeatureProperties, LabelProperties, Library
             if add == True:
                 material_list.append(mat_dic.copy())
 
+    return material_list
+
+def ML_Data_Save(Properties, Limit=10000, SearchParameter = ''):
+
+    import csv
+
+    load_dotenv()
+
+    materials_list = Mat_Search(Properties, SearchParameter, Limit, getname=True)
+
+    mat_matrix = [list(materials_list[0].keys())]
+
+    for mat in materials_list:
+
+        mat_matrix.append(list(mat.values()))
+
+    data_file = open(f"material_data_{SearchParameter}_{Limit}.csv", "a", newline='')
+
+    data_writer = csv.writer(data_file)
+
+    data_writer.writerows(mat_matrix)
+
+    data_file.close()
+
+def ML_Data_Process(FeatureProperties, LabelProperties, Library, SearchParameter='', Preprocess=False, limit=10000):
+    
+    #SKLEARN uses array like objects for fit model
+
+    #TENSORFLOW uses pandas.core.frame.DataFrame
+
+    #PYTORCH uses torchvision.datasets.mnist.FasionMNIST
+
+    load_dotenv()
+
+    properties = FeatureProperties + LabelProperties
+
+    material_list = Mat_Search(properties, SearchParameter, limit)
+
     feature_list = []
     label_list = []
-
-    print(material_list)
 
     for material in material_list:
 
@@ -233,6 +265,7 @@ def ML_Data_Process(SearchParameter, FeatureProperties, LabelProperties, Library
 
     #Format for raw data = tuple with features as list of lists as first element, label values for second element
     return data_process_functions[Library]("regression", raw_data) """
+
 
 
 
